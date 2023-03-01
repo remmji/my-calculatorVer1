bbfabcf9c6a6cc98caa7ca856de753410faa4480
remmji <remek6@gmail.com>
2023-03-01 22:32:59 +0100

    commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [display,setDisplay] = useState('');
+  
+
+  const handleClick = (input) =>{
+    
+    setDisplay(display+input);
+  }
+  const handleClear =()=>{
+    setDisplay('')
+  }
+  const handleEv = (display) => {
+    setDisplay(eval(display).toString());
+    console.log(display);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <section className="wraper">
+        <div className="container">
+            <input type="text" value={display} className="input" disabled/>
+        </div>
+        <div className="container">
+            <div className="box clear"><button className="btn clear" onClick={handleClear}>C</button></div>
+            <div className="box"><button className="btn fun" onClick={(()=>handleClick('%'))}>%</button></div>
+            <div className="box"><button className="btn fun" onClick={()=>handleClick("/")}>/</button></div>
+        </div>
+    <div className="container">
+        <div className="box"><button className="btn" onClick={()=>handleClick('7')}>7</button></div>
+        <div className="box"><button className="btn" onClick={()=>handleClick('8')}>8</button></div>
+        <div className="box"><button className="btn" onClick={()=>handleClick('9')}>9</button></div>
+        <div className="box"><button className="btn fun" onClick={()=>handleClick('*')}>*</button></div>
     </div>
+    
+    <div className="container">
+        <div className="box"><button className="btn" onClick={()=>handleClick('4')}>4</button></div>
+        <div className="box"><button className="btn" onClick={()=>handleClick('5')}>5</button></div>
+        <div className="box"><button className="btn" onClick={()=>handleClick('6')}>6</button></div>
+        <div className="box"><button className="btn fun" onClick={()=>handleClick('-')}>-</button></div>
+    </div>
+    <div className="container">
+        <div className="box"><button className="btn" onClick={()=>handleClick('1')}>1</button></div>
+        <div className="box"><button className="btn" onClick={()=>handleClick('2')}>2</button></div>
+        <div className="box"><button className="btn" onClick={()=>handleClick('3')}>3</button></div>
+        <div className="box"><button className="btn fun"onClick={()=>handleClick('+')} >+</button></div>
+    </div>
+    <div className="container">
+        <div className="box zero"><button className="btn zero" onClick={()=>handleClick('0')}>0</button></div>
+        <div className="box"><button className="btn" onClick={()=>handleClick('.')}>.</button></div>
+        <div className="box"><button className="btn equal"onClick={()=>handleEv(display)} >=</button></div>
+    </div>
+    </section>
+    </>
   );
 }
 
